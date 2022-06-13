d367ce00ab3f11ff955294acce83c8917a1ef70f
Ilenkov Vladislav <wowdogexlol@yandex.ru>
2022-06-13 21:46:59 +0300

    chetverty

--- main.c
+++ main.c
@@ -3,9 +3,18 @@
 int main(){
 int a;
 int b;
+int sum;
 
+printf("\nVvedite a =");
 scanf("%d",&a);
+
+
+printf("\nVvedite a =");
 scanf("%d",&b);
+
+sum = a + b;
+
+printf("\nSum = a + b = %d", sum);
 
 
 }
